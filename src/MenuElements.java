dbbe5fd2958df747d3adbacc0a8ff6d6ac699d03
Minh Tran <mct17@students.uwf.edu>
2024-03-04 18:54:41 -0600

    added action listeners and image io

--- src/MenuElements.java
+++ src/MenuElements.java
@@ -10,15 +10,22 @@ public class MenuElements extends JFrame {
 	private JButton startButton = new JButton("Start Game");
 	private JButton quitButton = new JButton("Quit Game");
 	private JButton settingsButton = new JButton("Settings");
-	private JButton startButton = new JButton("Instructions");
+	private JButton instructionsButton = new JButton("Instructions");
 	private static BufferedImage menuBackground;
 	
 	public MenuElements() {
-		
+		startButton.addActionListener(this);
+		quitButton.addActionListener(this);
+		settingsButton.addActionListener(this);
+		instructionsButton.addActionListener(this);
 	}
 	
 	public void paintElements(Graphics g) {
 		
+		Graphics2D g2d = (Graphics2D) g;
+		try {
+			menuBackground = ImageIO.read(new File("images/chalkboardStockPhoto.jpg"));
+		} catch(IOException e) {}
 	}
 	
 	public void actionPerformed(ActionEvent e) {
