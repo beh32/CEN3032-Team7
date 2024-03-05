d2f865d69e900dd44e0414b1a719c1fac28e46d2
Minh Tran <mct17@students.uwf.edu>
2024-03-04 18:45:40 -0600

    MenuElements skeleton created, background image added

---
+++ src/MenuElements.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Image.BufferedImage;
+
+public class MenuElements extends JFrame {
+
+	private JButton startButton = new JButton("Start Game");
+	private JButton quitButton = new JButton("Quit Game");
+	private JButton settingsButton = new JButton("Settings");
+	private JButton startButton = new JButton("Instructions");
+	private static BufferedImage menuBackground;
+	
+	public MenuElements() {
+		
+	}
+	
+	public void paintElements(Graphics g) {
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
