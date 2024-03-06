df9a9af031d2d6288f80e1578cd7b42098d7c0a3
Jack Anderson <jack.w.anderson2020@gmail.com>
2024-03-06 13:51:26 -0600

    Added Action Listeners and commented in what they will each do

--- HangmanUI.java
+++ HangmanUI.java
@@ -8,6 +8,8 @@ import javax.swing.JTextField;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class HangmanUI {
     public static void main(String[] args) {
@@ -17,7 +19,14 @@ public class HangmanUI {
         //Panel for the level, navigation controls, pause/play
         JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 80, 10));
             ImageIcon backIcon = new ImageIcon("res/angle-left.png");
-            JButton back = new JButton(backIcon); 
+            JButton back = new JButton(backIcon);
+            back.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    // This button would take you back to the main menu I am assuming
+                    // We need to decide how we plan on rendering each of the different screens
+                    // In the past, I have used a layered pane to swap between them
+                }
+            }); 
             topPanel.add(back);
 
             JLabel level = new JLabel("Level 1");
@@ -26,6 +35,11 @@ public class HangmanUI {
 
             ImageIcon pauseIcon = new ImageIcon("res/pause.png");
             JButton pause = new JButton(pauseIcon); 
+            pause.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    // Not sure what the pause button is for but the logic for that goes in here
+                }
+            });
             topPanel.add(pause);
 
         //Panel for the stick figure and word
@@ -33,6 +47,9 @@ public class HangmanUI {
             ImageIcon stickFigureIcon = new ImageIcon("res/example.png");
             JLabel exampleStickFigure = new JLabel(stickFigureIcon);
             centerPanel.add(exampleStickFigure);
+            /* Once we integrate all of the current branches to main, using the random word getter
+            class to generate our word whenever you press the play button and then we get the correct
+            number of blanks back to populate this JLabel */
             JLabel wordBlanks = new JLabel("_ _ _ _ _ _");
             wordBlanks.setFont(new Font("Sans-serif", Font.BOLD, 60));
             centerPanel.add(wordBlanks);
@@ -56,6 +73,17 @@ public class HangmanUI {
             bottomPanel.add(guessLabel);
             JTextField guessField = new JTextField(1);
             guessField.setFont(new Font("Sans-serif", Font.BOLD, 40));
+            guessField.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    // In here is where we will pass the input over to the validation
+                    // InputValidation.validLetter(guessField.getText().charAt(0));
+
+                    // After the input get validated we will then check if the letter is in the word
+                    // InputValidation.wordContains(guessField.getText().charAt(0));
+
+                    // Then we have to render either a new hangman limb or the letter on the correct lines
+                }
+            });
             bottomPanel.add(guessField);
 
         //Set layout manager for the frame
