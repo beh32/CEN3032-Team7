4d36ce66e41f0231234faaa490ca056e861d9e53
Minh Tran <mct17@students.uwf.edu>
2024-03-05 20:45:06 -0600

    dummy main added for testing, ui adjusted

--- src/MenuElements.java
+++ src/MenuElements.java
@@ -53,6 +53,13 @@ public class MenuElements extends JComponent implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == quitButton) {
 			System.exit(0);
+		} else if(e,getSource() == startButton) {
+			//START HANGMAN GAME
+		} else if(e,getSource() == settingsButton) {
+			
+		} else if(e,getSource() == instructionsButton) {
+			
 		}
+		
 	}
 }
