0387db1899b2cb2b2058ce512baab3daed86e832
Paytonwhite <143127902+Paytonwhite@users.noreply.github.com>
2024-03-07 02:27:19 -0600

    Finished Cleaning HangmanController/InputValidation

--- HangmanController.java
+++ HangmanController.java
@@ -4,71 +4,93 @@ import java.util.Set;
 
 public class HangmanController {
 
-    private Set<Character> usedLetters;
-    private String hangmanWord;
-    private String hiddenWord;
-    private int figureCount;
-    private char userInput;
+    private String wordToGuess;
+    private String wordDisplayString; 
     private boolean wordGuessed;
-
-    public HangmanController() {
-        usedLetters = new HashSet<>(26);
-        hangmanWord = "amazing"; // default word use wordbank
-        hiddenWord = "";
-
-        for (int i = 0; i < hangmanWord.length(); ++i)
-            hiddenWord += "_";
-
-        figureCount = 0;
-        wordGuessed = false;
-    }
-
+    private int hangmanPartsDrawn;
+    private char userInput;
+    private Set<Character> usedLetters;
+    private Scanner scan;
     
     public void hangmanRound() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Round begin");
+        initializeRound();
+        getWord(); 
+        
+        while (hangmanPartsDrawn <=6 && !wordGuessed) {
+            System.out.println(wordDisplayString); //Replace with GUI
+            System.out.println(hangmanPartsDrawn); 
 
-        while (figureCount <=6 && !wordGuessed) {
-            System.out.println(hiddenWord);
-            System.out.println(figureCount);
-
-            userInput = scan.next().charAt(0);
-            
-            if (Character.isLetter(userInput)  && !usedLetters.contains(userInput)) {
-                    usedLetters.add(userInput);
-                
-                    if (hangmanWord.contains(Character.toString(userInput))) {
-                        // Add stuff for word being correct here. 
-                        int index = hangmanWord.indexOf(userInput);
-                        while(index >= 0) {
-                            System.out.println(index);
-                            hiddenWord = hiddenWord.substring(0, index) + userInput + hiddenWord.substring(index+1);
-                            index = hangmanWord.indexOf(userInput, index+1);
-                        }                       
-                        
-                        if (hiddenWord.equals(hangmanWord))
-                            wordGuessed = true;
-
-                    }
-                    
-                    else 
-                        ++figureCount;
-                    
-                }
-
-            else {
-                System.out.println("Cannot use this letter!");
-            }
+            userInput = scan.next().charAt(0);  
+            validateUserInput();
 
         }
-        
-        if (wordGuessed) 
-            System.out.println(hiddenWord + "\nYou Won!");
-        else
-            System.out.println(hangmanWord + "\nYou Lost :(");
 
-        scan.close();
+        endRound();
+
+    }
+
+    private void initializeRound() {
+        hangmanPartsDrawn = 0;
+        wordGuessed = false;
+        usedLetters = new HashSet<>(26);
+        scan = new Scanner(System.in);
+        System.out.println("Round begin");
+    }
+
+    private void getWord() {
+        wordToGuess = "amazing"; // default word use wordbank class for wordToGuess/wordDisplayString
+        wordDisplayString = "";
+
+        for (int i = 0; i < wordToGuess.length(); ++i) //blank spaces equal to word length 
+            wordDisplayString += "_";
+
+    }
+
+    private void validateUserInput() {
+        if (Character.isLetter(userInput)  && !usedLetters.contains(userInput)) { // input is letter but not used.
+            usedLetters.add(userInput);
+            
+            if (wordToGuess.contains(Character.toString(userInput))) { //check if letter is in word             
+                editWordDisplay();
+            
+                if (wordDisplayString.equals(wordToGuess)) 
+                    wordGuessed = true;
+
+            }
+                          
+            else 
+                ++hangmanPartsDrawn;
+                // Add hangman UI drawing here
+
+        }
+
+        else 
+            System.out.println("Cannot use this letter!");
         
+    }    
+
+    private void editWordDisplay() {
+        int index = wordToGuess.indexOf(userInput); // Swap code out for word display
+                    
+                while(index >= 0) {
+                    System.out.println(index);
+                    wordDisplayString = wordDisplayString.substring(0, index) + userInput + wordDisplayString.substring(index+1);
+                    index = wordToGuess.indexOf(userInput, index+1);
+                }   
+    }
+
+    private void endRound() {
+        scan.close();
+
+        if (wordGuessed) 
+            System.out.println(wordDisplayString + "\nYou Won!");
+        else
+            System.out.println(wordToGuess + "\nYou Lost :(");
+            
+    }
+
+    public boolean isWordGuessed() {
+        return wordGuessed;
     }
 
 }
