3a1bb0ec3cdd5b1e041e51e82c3e6f804662c05a
bailey <144845660+beh32@users.noreply.github.com>
2024-03-06 22:32:04 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
         // Create an instance of Main Menu
-        HangmanMenu.main(args);
+        HangmanMenu hangmanMenu = new HangmanMenu();
+        hangmanMenu.HangmanMenu();
     }
 }
