b28c3a57b71f53189404d115e545385a34c91720
Minh Tran <mct17@students.uwf.edu>
2024-04-03 20:12:41 -0500

    added buffered reader to difficulty class, moved HangmanController

--- src/Difficulty.java
+++ src/Difficulty.java
@@ -2,17 +2,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DIfficulty {
-	
+	String fileName = "wordbank.txt";
 	private List<String> easyWords;
 	private List<String> mediumWords;
 	private List<String> hardWords;
 	
 	public Difficulty(List<String> easyWords, List<String> mediumWords, List<String> hardWords) {
-		this.easyWords = easyWords;
-		this.mediumWords = mediumWords;
-		this.hardWords = hardWords;
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+			String line;
+			//sort words by length
+			if(!line.isEmpty()) {
+			if(line.length() <= 5) {
+				easyWords.add(line);
+			} else if (line.length() <= 7) {
+				mediumWords.add(line);
+			} else {
+				hardWords.add(line);
+			}
+			}
+		} catch(IOException e) {}
 	}
-	
+		
 	public List<String> getEasyWords() {
 		return easyWords;
 	}
--- src/HangmanController.java
+++ src/HangmanController.java
@@ -1,27 +1,102 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
-public class DIfficulty {
-	
-	private List<String> easyWords;
-	private List<String> mediumWords;
-	private List<String> hardWords;
-	
-	public Difficulty(List<String> easyWords, List<String> mediumWords, List<String> hardWords) {
-		this.easyWords = easyWords;
-		this.mediumWords = mediumWords;
-		this.hardWords = hardWords;
-	}
-	
-	public List<String> getEasyWords() {
-		return easyWords;
-	}
-	
-	public List<String> getMediumWords() {
-		return mediumWords;
-	}
-	
-	public List<String> getHardWords() {
-		return hardWords;
-	}
+public class HangmanController {
+
+    private String wordToGuess;
+    private String wordDisplayString; 
+    private boolean wordGuessed;
+    private int hangmanPartsDrawn;
+    private char userInput;
+    private Set<Character> usedLetters;
+    private Scanner scan;
+    
+    public void hangmanRound() {
+        initializeRound();
+        getWord(); 
+        
+        while (hangmanPartsDrawn <=6 && !wordGuessed) {
+            System.out.println(wordDisplayString); //Replace with GUI
+            System.out.println("Hangman Body Parts Draw: " + hangmanPartsDrawn); 
+            System.out.print("Guess: ");
+            userInput = scan.next().charAt(0);  
+            validateUserInput();
+
+        }
+
+        endRound();
+
+    }
+
+    private void initializeRound() {
+        hangmanPartsDrawn = 0;
+        wordGuessed = false;
+        usedLetters = new HashSet<>(26);
+        scan = new Scanner(System.in);
+        System.out.println("Round begin");
+    }
+
+    private void getWord() {
+        wordToGuess = "amazing"; // default word use wordbank class for wordToGuess/wordDisplayString
+        wordDisplayString = "";
+
+        for (int i = 0; i < wordToGuess.length(); ++i) //blank spaces equal to word length 
+            wordDisplayString += "_";
+
+    }
+
+    private void validateUserInput() {
+        if (Character.isLetter(userInput)  && !usedLetters.contains(userInput)) { // input is letter but not used.
+            usedLetters.add(userInput);
+            
+            if (wordToGuess.contains(Character.toString(userInput))) { //check if letter is in word             
+                System.out.println(userInput + " is correct!");
+                editWordDisplay();
+
+            
+                if (wordDisplayString.equals(wordToGuess)) 
+                    wordGuessed = true;
+
+            }
+                          
+            else {
+                System.out.println(userInput + " is incorrect!");
+                ++hangmanPartsDrawn;
+                // Add hangman UI drawing here
+            }
+        }
+
+        else 
+            System.out.println("Cannot use this letter!");
+        
+    }    
+
+    private void editWordDisplay() {
+        int index = wordToGuess.indexOf(userInput); // Swap code out for word display
+                    
+                while(index >= 0) { // find all instances
+                    wordDisplayString = wordDisplayString.substring(0, index) + userInput + wordDisplayString.substring(index+1);
+                    index = wordToGuess.indexOf(userInput, index+1);
+                }   
+    }
+
+    private void endRound() {
+        scan.close();
+
+        if (wordGuessed) 
+            System.out.println(wordDisplayString + "\nYou Won!");
+        else
+            System.out.println(wordToGuess + "\nYou Lost :(");
+            
+    }
+
+    public boolean isWordGuessed() {
+        return wordGuessed;
+    }
+
+    public boolean isGameOver() {
+        return wordGuessed || hangmanPartsDrawn >= 6;
+    }
+
 }
